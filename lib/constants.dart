853a385792efd216953e36a0dbf7a7f2628057d4
alaaqz <alaa.qzaih@gmail.com>
2022-08-14 22:00:12 +0300

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = Color(0xffE63220);
+
+enum Option { LogIn, SignUp }
---
+++ lib/signup.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:login_and_signup_web/constants.dart';
+import 'package:login_and_signup_web/action_button.dart';
+
+class SignUp extends StatefulWidget {
+  final Function onLogInSelected;
+
+  SignUp({@required this.onLogInSelected});
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: EdgeInsets.all(size.height > 770
+          ? 64
+          : size.height > 670
+              ? 32
+              : 16),
+      child: Center(
+        child: Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(25),
+            ),
+          ),
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            height: size.height *
+                (size.height > 770
+                    ? 0.7
+                    : size.height > 670
+                        ? 0.8
+                        : 0.9),
+            width: 500,
+            color: Colors.white,
+            child: Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(40),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        " تسجيل الدخول كمزود خدمة",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Container(
+                        width: 30,
+                        child: Divider(
+                          color: kPrimaryColor,
+                          thickness: 2,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'رقم الهاتف',
+                          labelText: 'رقم الهاتف',
+                          suffixIcon: Icon(
+                            Icons.phone,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 32,
+                      ),
+
+                      /* TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          labelText: 'Email',
+                          suffixIcon: Icon(
+                            Icons.mail_outline,
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 32,
+                      ),*/
+
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'كلمة السر',
+                          labelText: 'كلمة السر',
+                          suffixIcon: Icon(
+                            Icons.lock_outline,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 64,
+                      ),
+                      actionButton("تسجيل الدخول"),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "تسجيل الدخول كمدير",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 14,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              widget.onLogInSelected();
+                            },
+                            child: Row(
+                              children: [
+                                Text(
+                                  " تسجيل الدخول",
+                                  style: TextStyle(
+                                    color: kPrimaryColor,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Icon(
+                                  Icons.arrow_forward,
+                                  color: kPrimaryColor,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
